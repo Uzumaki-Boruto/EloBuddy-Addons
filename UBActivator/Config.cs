61e19a74cc1218f97e1ba6a7171131ba9e5612c3
Uzumaki Boruto <bbe.haminte@gmail.com>
2016-09-18 19:59:54 +0700

    Add auto level up Taliyah & Kled

--- UBActivator/Config.cs
+++ UBActivator/Config.cs
@@ -501,6 +501,10 @@ namespace UBActivator
                         SkillOrder = new[] { 1, 3, 2, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
                         break;
 
+                    case "Kled":
+                        SkillOrder = new[] { 1, 2, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
+                        break;
+
                     case "Karma":
                         Extensions.ROff = -1;
                         SkillOrder = new[] { 1, 3, 1, 2, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
@@ -716,6 +720,10 @@ namespace UBActivator
 
                     case "Syndra":
                         SkillOrder = new[] { 1, 3, 2, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
+                        break;
+
+                    case "Taliyah":
+                        SkillOrder = new[] { 1, 3, 2, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
                         break;
 
                     case "Talon":
