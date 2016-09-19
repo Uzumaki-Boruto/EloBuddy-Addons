5b53416da4c4f1ce05d1ddb4f6a9c0686a2e0334
Uzumaki Boruto <bbe.haminte@gmail.com>
2016-09-19 12:45:08 +0700

    Fix Not Use Radiun

--- UBActivator/Combat.cs
+++ UBActivator/Combat.cs
@@ -14,7 +14,7 @@ namespace UBActivator
                 || !Config.Combat["GloryCb"].Cast<CheckBox>().CurrentValue))
             {
                 if (Player.Instance.CountEnemiesInRange(Player.Instance.MoveSpeed * 2) < 1
-                    || Config.Combat["Glorycountally"].Cast<Slider>().CurrentValue > Player.Instance.CountAlliesInRange(500)) return;
+                    || Config.Combat["Glorycountally"].Cast<Slider>().CurrentValue < Player.Instance.CountAlliesInRange(500)) return;
 
                 if (Items.Righteous_Glory != null)
                 {
@@ -26,12 +26,11 @@ namespace UBActivator
                 && ((Config.Combat["RanduinCb"].Cast<CheckBox>().CurrentValue && Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
                 || !Config.Combat["RanduinCb"].Cast<CheckBox>().CurrentValue))
             {
-                if (Player.Instance.CountEnemiesInRange(Player.Instance.MoveSpeed * 2) < 1
-                    || Config.Combat["Glorycountally"].Cast<Slider>().CurrentValue > Player.Instance.CountAlliesInRange(500)) return;
+                if (Config.Combat["Randuincount"].Cast<Slider>().CurrentValue < Player.Instance.CountEnemiesInRange(500)) return;
 
-                if (Items.Righteous_Glory != null)
+                if (Items.Randuin_s_Omen != null)
                 {
-                    if (Items.Righteous_Glory.IsOwned() && Items.Righteous_Glory.IsReady()) Items.Righteous_Glory.Cast();
+                    if (Items.Randuin_s_Omen.IsOwned() && Items.Randuin_s_Omen.IsReady()) Items.Randuin_s_Omen.Cast();
                 }
             }
         }
