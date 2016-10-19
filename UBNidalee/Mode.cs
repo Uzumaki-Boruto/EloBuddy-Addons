13acec4762ecb96c82780d501e9700a74686da1e
Uzumaki Boruto <bbe.haminte@gmail.com>
2016-10-19 21:23:19 +0700

    Collision

--- UBNidalee/Mode.cs
+++ UBNidalee/Mode.cs
@@ -24,7 +24,7 @@ namespace UBNidalee
                     if (target != null)
                     {
                         var Qgp = Spells.Q.GetPrediction(target);
-                        if (target.IsValidTarget() && Qgp.HitChancePercent >= Qhitchance)
+                        if (target.IsValidTarget() && Qgp.HitChancePercent >= Qhitchance && Qgp.CollisionObjects.Count() == 0)
                         {
                             Spells.Q.Cast(Qgp.UnitPosition);
                         }
@@ -284,7 +284,7 @@ namespace UBNidalee
                     if (target != null)
                     {
                         var Qgp = Spells.W.GetPrediction(target);
-                        if (target != null && target.IsValidTarget() && Qgp.HitChancePercent >= Qhitchance)
+                        if (target != null && target.IsValidTarget() && Qgp.HitChancePercent >= Qhitchance && Qgp.CollisionObjects.Count() == 0)
                         {
                             Spells.Q.Cast(Qgp.CastPosition);
                         }
@@ -441,6 +441,7 @@ namespace UBNidalee
                     if (target != null)
                     {
                         var pred = Spells.Q.GetPrediction(target);
+                        if (pred.CollisionObjects.Count() == 0)
                         {
                             Spells.Q.Cast(pred.CastPosition);
                         }
